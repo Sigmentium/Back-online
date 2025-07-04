99b41bb63161e63afc595c9a26454555daaeab2c
MarkushaRaz <LegoAnimation2022@gmail.com>
2025-07-04 20:14:45 +0300

    3

--- Main.js
+++ Main.js
@@ -11,7 +11,7 @@ let BullsCows = [];
 // function GenerateId() {}
 
 function Send(to, type, data) {
-    const data = JSON.parse(data);
+    data = JSON.parse(data);
 
     switch (type) {
         case 'Info-Tsu-E-Fa':
